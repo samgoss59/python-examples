c4bab35123119de7fed518ebc6932983ae6a89e1
samgoss59 <69603391+samgoss59@users.noreply.github.com>
2020-08-31 20:51:18 -0400

    Add files via upload
    files from 8/31 class

---
+++ break.py
@@ -0,0 +1,8 @@
+i = 0
+while i < 10:
+      if i % 2 ==0:
+        print("found an even number:", i)
+        i += 1
+        continue
+        break
+    i += 1
---
+++ cont.py
@@ -0,0 +1,5 @@
+for i in range(0,9):
+      if i % 2 ==0:
+        print("found an even number:", i)
+        continue
+ 
